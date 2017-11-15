f308bbb2dab8929095799f60abf734382d60683e
Mary Clare Harrington <maryclareharrington@Marys-MacBook-Air.local>
2017-11-14 20:15:08 -0500

    lab 4 part 1

---
+++ scripts/controller.js
@@ -0,0 +1,17 @@
+(function(){
+function FormController() {
+	var vm = this;
+	vm.tasks = [];
+	vm.addTask = function(newItem) {
+		console.log(newItem);
+		vm.tasks.push(newItem);
+	};
+	vm.removeItem = function(index) {
+		vm.tasks.splice(index, 1);	
+	};
+}	
+	
+angular
+	.module("app")
+	.controller("FormController", FormController);
+})();
